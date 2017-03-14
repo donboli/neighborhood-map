8df89f278938bbfe066913de95ac0cdc2829993e
Juan Becker <wolli.becker@gmail.com>
2017-03-14 19:29:04 +0100

    Add dummy configuration file

---
+++ dummy_config.js
@@ -0,0 +1,12 @@
+/*
+  This is a dummy configuration file.
+  Copy this file and rename it to config.js.
+  Then fill in your credentials to make the app work.
+*/
+
+module.exports = {
+  consumer_key: 'consumer_key',
+  consumer_secret: 'secret',
+  token: 'token',
+  token_secret: 'request_token'
+};
